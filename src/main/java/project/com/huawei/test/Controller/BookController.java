22dddf02622dd257dc0ad0b35735e0c99fbcb64b
zouqiang <604708222@qq.com>
2019-12-24 10:16:31 +0800

    Redis

--- src/main/java/project/com/huawei/test/Controller/BookController.java
+++ src/main/java/project/com/huawei/test/Controller/BookController.java
@@ -32,185 +32,196 @@ import project.com.huawei.test.entity.Book;
 @Controller
 @RequestMapping("/book")
 public class BookController {
-    
+
     @Resource
     private BookDao bookDao;
 
-    
     @Resource
     private JdbcTemplate jdbcTemplate;
-    
+
     /**
      * 查找所有图书
      */
     @RequestMapping("/list")
-    public ModelAndView list(){
+    public ModelAndView list() {
+        System.out.println(66666);
         ModelAndView mav = new ModelAndView();
         List<Book> bookList = bookDao.findAll();
         mav.addObject("bookList", bookList);
         mav.setViewName("bookList");
         return mav;
     }
-    
+
     /**
      * 添加图书
      */
-    @RequestMapping(value="/add", method=RequestMethod.POST)
-    public String add(Book book){
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String add(Book book) {
         bookDao.save(book);
         return "redirect:/book/list";
     }
-    
+
     /**
      * 根据id查找图书
      */
     @RequestMapping("/preUpdate/{id}")
-    public ModelAndView preUpdate(@PathVariable("id") Integer id){
+    public ModelAndView preUpdate(@PathVariable("id") Integer id) {
         ModelAndView mav = new ModelAndView();
         mav.addObject("book", bookDao.getOne(id));
         mav.setViewName("bookUpdate");
         return mav;
     }
-    
+
     /**
      * 修改图书
      */
     @PostMapping("/update")
-    public String update(Book book){
-        bookDao.save(book); //依然是save，有id就修改，没有id就添加
+    public String update(Book book) {
+        bookDao.save(book); // 依然是save，有id就修改，没有id就添加
         return "redirect:/book/list";
     }
-    
+
     /**
      * 修改图书
      */
     @RequestMapping("/bookAdd")
-    public ModelAndView bookAdd(Book book){
-    	ModelAndView mav = new ModelAndView();
+    public ModelAndView bookAdd(Book book) {
+        ModelAndView mav = new ModelAndView();
         mav.setViewName("bookAdd");
         return mav;
     }
-    
+
     /**
      * 删除图书
      */
     @GetMapping("/delete")
-    public String delete(Integer id){
+    public String delete(Integer id) {
         bookDao.delete(id);
         return "redirect:/book/list";
     }
+
     /**
      * 删除图书
      */
     @GetMapping("/testquery")
-    public String testquery(){
-    	test10();
-    	return "redirect:/book/list";
+    public String testquery() {
+        test10();
+        return "redirect:/book/list";
     }
-    
-    private void test1(){
-    	Pageable page = new PageRequest(0, 2);
-    	Page<Book> p = bookDao.findAll(page);
-    	List<Book> list = p.getContent();
-    	list.forEach(System.out::println);
-    	System.out.println("总条数为："+p.getTotalElements());
-    	System.out.println("总页数为："+p.getTotalPages());
+
+    private void test1() {
+        Pageable page = new PageRequest(0, 2);
+        Page<Book> p = bookDao.findAll(page);
+        List<Book> list = p.getContent();
+        list.forEach(System.out::println);
+        System.out.println("总条数为：" + p.getTotalElements());
+        System.out.println("总页数为：" + p.getTotalPages());
     }
-    private void test2(){
-    	Sort sort = new Sort(Direction.DESC,"id");
-    	List<Book> list = bookDao.findAll(sort);
-    	list.forEach(System.out::println);
+
+    private void test2() {
+        Sort sort = new Sort(Direction.DESC, "id");
+        List<Book> list = bookDao.findAll(sort);
+        list.forEach(System.out::println);
     }
-    
-    private void test3(){
-    	Order order1 = new Order(Direction.DESC,"id");
-    	Order order2 = new Order(Direction.ASC,"author");
-    	Sort sort = new Sort(order1,order2);
-    	List<Book> list = bookDao.findAll(sort);
-    	list.forEach(System.out::println);
+
+    private void test3() {
+        Order order1 = new Order(Direction.DESC, "id");
+        Order order2 = new Order(Direction.ASC, "author");
+        Sort sort = new Sort(order1, order2);
+        List<Book> list = bookDao.findAll(sort);
+        list.forEach(System.out::println);
     }
-    
-    private void test4(){
-    	
-    	List<Book> list = bookDao.findAll((Root<Book> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> {
-    		return cb.equal(root.get("author"), "zuozhe3");
-    	} );
-    	list.forEach(System.out::println);
+
+    private void test4() {
+
+        List<Book> list = bookDao.findAll((Root<Book> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> {
+            return cb.equal(root.get("author"), "zuozhe3");
+        });
+        list.forEach(System.out::println);
     }
-    private void test5(){
-    	
-    	List<Book> list = bookDao.findAll((Root<Book> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> {
-    		List<Predicate> listp = new ArrayList<>();
-    		listp.add(cb.like(root.get("author"), "%zuozhe%"));
-    		listp.add(cb.like(root.get("name"), "%shu%"));
-    		
-    		return cb.and(listp.toArray(new Predicate[listp.size()]));
-    	} );
-    	list.forEach(System.out::println);
+
+    private void test5() {
+
+        List<Book> list = bookDao.findAll((Root<Book> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> {
+            List<Predicate> listp = new ArrayList<>();
+            listp.add(cb.like(root.get("author"), "%zuozhe%"));
+            listp.add(cb.like(root.get("name"), "%shu%"));
+
+            return cb.and(listp.toArray(new Predicate[listp.size()]));
+        });
+        list.forEach(System.out::println);
     }
-    private void test6(){
-    	
-    	List<Book> list = bookDao.findAll((Root<Book> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> {
-    		List<Predicate> listp = new ArrayList<>();
-    		listp.add(cb.like(root.get("author"), "%zuozhe%"));
-    		listp.add(cb.like(root.get("name"), "%shu%"));
-    		
-    		return cb.and(cb.like(root.get("author"), "%zuozhe%"),cb.like(root.get("name"), "%shu%"));
-    	} );
-    	list.forEach(System.out::println);
+
+    private void test6() {
+
+        List<Book> list = bookDao.findAll((Root<Book> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> {
+            List<Predicate> listp = new ArrayList<>();
+            listp.add(cb.like(root.get("author"), "%zuozhe%"));
+            listp.add(cb.like(root.get("name"), "%shu%"));
+
+            return cb.and(cb.like(root.get("author"), "%zuozhe%"), cb.like(root.get("name"), "%shu%"));
+        });
+        list.forEach(System.out::println);
     }
-    private void test7(){
-    	
-    	Page<Book> list = bookDao.findAll((Root<Book> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> {
-    		List<Predicate> listp = new ArrayList<>();
-    		listp.add(cb.like(root.get("author"), "%zuozhe%"));
-    		listp.add(cb.like(root.get("name"), "%shu%"));
-    		
-    		return cb.and(cb.like(root.get("author"), "%zuozhe%"),cb.like(root.get("name"), "%shu%"));
-    	},new PageRequest(0, 2) );
-    	list.getContent().forEach(System.out::println);
-    	System.out.println(list.getTotalElements());
-    	System.out.println(list.getTotalPages());
+
+    private void test7() {
+
+        Page<Book> list = bookDao.findAll((Root<Book> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> {
+            List<Predicate> listp = new ArrayList<>();
+            listp.add(cb.like(root.get("author"), "%zuozhe%"));
+            listp.add(cb.like(root.get("name"), "%shu%"));
+
+            return cb.and(cb.like(root.get("author"), "%zuozhe%"), cb.like(root.get("name"), "%shu%"));
+        }, new PageRequest(0, 2));
+        list.getContent().forEach(System.out::println);
+        System.out.println(list.getTotalElements());
+        System.out.println(list.getTotalPages());
+
     }
-    private void test8(){
-    	
-    	List<Book> list = bookDao.findAll((Root<Book> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> {
-    		List<Predicate> listp = new ArrayList<>();
-    		listp.add(cb.like(root.get("author"), "%zuozhe%"));
-    		listp.add(cb.like(root.get("name"), "%shu%"));
-    		return cb.and(cb.like(root.get("author"), "%zuozhe%"),cb.like(root.get("name"), "%shu%"));
-    	},new Sort(new Order(Direction.DESC,"id"),new Order(Direction.ASC,"author")));
-    	list.forEach(System.out::println);
+
+    private void test8() {
+
+        List<Book> list = bookDao.findAll((Root<Book> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> {
+            List<Predicate> listp = new ArrayList<>();
+            listp.add(cb.like(root.get("author"), "%zuozhe%"));
+            listp.add(cb.like(root.get("name"), "%shu%"));
+            return cb.and(cb.like(root.get("author"), "%zuozhe%"), cb.like(root.get("name"), "%shu%"));
+        }, new Sort(new Order(Direction.DESC, "id"), new Order(Direction.ASC, "author")));
+        list.forEach(System.out::println);
     }
-    private void test9(){
-    	
-    	Page<Book> list = bookDao.findAll((Root<Book> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> {
-    		List<Predicate> listp = new ArrayList<>();
-    		listp.add(cb.like(root.get("author"), "%zuozhe%"));
-    		listp.add(cb.like(root.get("name"), "%shu%"));
-    		return cb.and(cb.like(root.get("author"), "%zuozhe%"),cb.like(root.get("name"), "%shu%"));
-    	},new PageRequest(0, 2,new Sort(new Order(Direction.DESC,"id"),new Order(Direction.ASC,"author"))));
-    	list.getContent().forEach(System.out::println);
+
+    private void test9() {
+
+        Page<Book> list = bookDao.findAll((Root<Book> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> {
+            List<Predicate> listp = new ArrayList<>();
+            listp.add(cb.like(root.get("author"), "%zuozhe%"));
+            listp.add(cb.like(root.get("name"), "%shu%"));
+            return cb.and(cb.like(root.get("author"), "%zuozhe%"), cb.like(root.get("name"), "%shu%"));
+        }, new PageRequest(0, 2, new Sort(new Order(Direction.DESC, "id"), new Order(Direction.ASC, "author"))));
+        list.getContent().forEach(System.out::println);
     }
-    private void test10(){
-    	
-    	Integer count = jdbcTemplate.queryForObject("select count(*) from t_book t where t.name like ?", Integer.class, new Object[]{"%shu%"});
-    	System.out.println(count);
-    	Book book = jdbcTemplate.queryForObject("select * from t_book t where t.name like ?", (ResultSet r, int i) -> {
-    		return new Book().setName(r.getString("name")).setAuthor(r.getString("author")).setId(r.getInt("id"));
-    	}, new Object[]{"%shu4"});
-    	System.out.println(book);
-    	
-    	List<Map<String, Object>> list = jdbcTemplate.queryForList("select * from t_book t where t.name like ?", new Object[]{"%shu%"});
-    	list.forEach(System.out::println);
-    	List<Book> list1 = jdbcTemplate.query("select * from t_book t where t.name like ?",(ResultSet r, int i) -> {
-    		return new Book().setName(r.getString("name")).setAuthor(r.getString("author")).setId(r.getInt("id"));
-    	}, new Object[]{"%shu%"});
-    	System.out.println(list1);
-    	
-    	Map<String, Object> m = jdbcTemplate.queryForMap("select * from t_book t where t.name like ?", new Object[]{"%shu3%"});
-    	System.out.println(m);
+
+    private void test10() {
+
+        Integer count = jdbcTemplate.queryForObject("select count(*) from t_book t where t.name like ?", Integer.class,
+                new Object[] { "%shu%" });
+        System.out.println(count);
+        Book book = jdbcTemplate.queryForObject("select * from t_book t where t.name like ?", (ResultSet r, int i) -> {
+            return new Book().setName(r.getString("name")).setAuthor(r.getString("author")).setId(r.getInt("id"));
+        }, new Object[] { "%shu4" });
+        System.out.println(book);
+
+        List<Map<String, Object>> list = jdbcTemplate.queryForList("select * from t_book t where t.name like ?",
+                new Object[] { "%shu%" });
+        list.forEach(System.out::println);
+        List<Book> list1 = jdbcTemplate.query("select * from t_book t where t.name like ?", (ResultSet r, int i) -> {
+            return new Book().setName(r.getString("name")).setAuthor(r.getString("author")).setId(r.getInt("id"));
+        }, new Object[] { "%shu%" });
+        System.out.println(list1);
+
+        Map<String, Object> m = jdbcTemplate.queryForMap("select * from t_book t where t.name like ?",
+                new Object[] { "%shu3%" });
+        System.out.println(m);
     }
-    
-    
+
 }
---
+++ src/main/java/project/com/huawei/test/test/SpringbootDemo.java
@@ -0,0 +1,33 @@
+package project.com.huawei.test.test;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.TimeUnit;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootDemo {
+
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void  test(){
+        redisTemplate.setEnableTransactionSupport(false);
+        System.out.println("===========================================================");
+//        redisTemplate.expire("kk1",2, TimeUnit.HOURS);
+//        redisTemplate.opsForValue().set("kk1","vv1");
+
+        System.out.println(redisTemplate.opsForValue().get("kk1"));
+        System.out.println(redisTemplate.opsForHash().get("hash1","hk1"));
+
+    }
+
+}
